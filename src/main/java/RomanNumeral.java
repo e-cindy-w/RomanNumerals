4285c5de708c1f5ed488a6aa73b2d8b845a2b101
e-cindy-w <elsiecindy21@gmail.com>
2023-03-27 19:20:31 +0300

    First commit

---
+++ src/main/java/RomanNumeral.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Elsie Cindy
+ */
+public class RomanNumeral {
+    public int convertToInteger(String romanNumeral) {
+        if (romanNumeral == null || romanNumeral.isEmpty()) {
+            throw new IllegalArgumentException("Input cannot be null or empty.");
+        }
+
+        int result = 0;
+        int prev = 0;
+        
+        for (int i = romanNumeral.length() - 1; i >= 0; i--) {
+            
+            int current = romanNumeralToValue(romanNumeral.charAt(i));
+            if (current < prev) {
+                result -= current;
+            } else {
+                result += current;
+            }
+            prev = current;
+        }
+
+        if (result == 0) {
+            throw new IllegalArgumentException("Invalid input: " + romanNumeral);
+        }
+
+        return result;
+        
+    }
+
+    private int romanNumeralToValue(char c) {
+        switch (c) {
+            case 'I' -> {
+                return 1;
+            }
+            case 'V' -> {
+                return 5;
+            }
+            case 'X' -> {
+                return 10;
+            }
+            case 'L' -> {
+                return 50;
+            }
+            case 'C' -> {
+                return 100;
+            }
+            case 'D' -> {
+                return 500;
+            }
+            case 'M' -> {
+                return 1000;
+            }
+            default -> throw new IllegalArgumentException("Invalid input: ");
+        }
+    }
+    
+}
---
+++ src/test/java/RomanNumeralUnitTests.java
@@ -0,0 +1,122 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+//import java.lang.IllegalArgumentException;
+
+
+/**
+ *
+ * @author Elsie Cindy
+ */
+public class RomanNumeralUnitTests {
+
+    
+    public RomanNumeralUnitTests() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+    
+// Test cases
+//    Single Letters:I, V, X, L, C, D, M.
+//    Many Letters: XI
+//    Subtractive notation (SN):IV
+//    With and without SN:XIV
+//    Repetition:II
+//    First number:I
+//    inValid Letter:Z
+//    Invalid and valid Letter: XIZ
+//    Not Valid:VV
+//    Null:
+
+    @Test
+    public void firstLetter() {
+        RomanNumeral converter = new RomanNumeral();
+        int result = converter.convertToInteger("V");
+        assertEquals(5, result);
+    }
+    
+    @Test
+    public void singleLetter() {
+        RomanNumeral converter = new RomanNumeral();
+        int result = converter.convertToInteger("V");
+        assertEquals(5, result);
+    }
+    
+    @Test
+    public void repetition() {
+        int result = new RomanNumeral().convertToInteger("XX");
+        Assertions.assertEquals(20, result);
+     }
+    
+    @Test
+    public void manyLetters(){
+        int result = new RomanNumeral().convertToInteger("VII");
+        Assertions.assertEquals(7,result);
+    }
+    
+    @Test
+    public void subtractiveNotation(){
+        int result = new RomanNumeral().convertToInteger("IV");
+        Assertions.assertEquals(4,result);
+    }
+    
+    @Test
+    public void withNwithoutSN(){
+        int result = new RomanNumeral().convertToInteger("XIV");
+        Assertions.assertEquals(14,result);
+    }
+        
+    @Test
+    public void invalidLetter() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        assertThrows(IllegalArgumentException.class, () -> romanNumeral.convertToInteger("Y"));
+    }
+    
+         @Test
+    public void nullInput() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        assertThrows(IllegalArgumentException.class, () -> romanNumeral.convertToInteger(null));
+    }
+    
+    @Test
+    public void isEmptyInput() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        assertThrows(IllegalArgumentException.class, () -> romanNumeral.convertToInteger(" "));
+    }
+    
+    @Test
+    public void invalidAndValidLetter() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        assertThrows(IllegalArgumentException.class, () -> romanNumeral.convertToInteger("XIZ"));
+    }
+    
+////     @Test
+////    public void notVaild() {
+////        RomanNumeral romanNumeral = new RomanNumeral();
+////        assertThrows(IllegalArgumentException.class, () -> romanNumeral.convertToInteger("VV"));
+////    }
+//    
+}
